33c2d6b3392441a64e41814c8ab4618403a7c0e5
AlexandrRudenok <rudenok2008@mail.ru>
2022-08-31 10:52:00 +0300

    add new files

---
+++ Netology_IB_Instruments/Profile.xib/ProfileView.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Александр Руденок on 29.08.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    @IBOutlet weak var avatarka: UIImageView!
+    @IBOutlet weak var myName: UILabel!
+    @IBOutlet weak var myAge: UILabel!
+    @IBOutlet weak var myCity: UILabel!
+    @IBOutlet weak var myText: UITextView!
+    
+}
---
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Александр Руденок on 29.08.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        loadNib()
+    }
+    
+    func loadNib() {
+        if let xibView = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? ProfileView {
+            xibView.frame = CGRect(x: 0, y: 158, width: UIScreen.main.bounds.width, height: 300)
+            view.addSubview(xibView)
+        }
+    }
+}
